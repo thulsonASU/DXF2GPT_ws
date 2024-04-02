95248fa45a34c3bd794ef28d5ffa0a73039b7392
Tyler Hulson <thulson00@gmail.com>
2024-04-01 20:08:01 -0700

    2-opt TSP solver

--- src/DXF2GPT.py
+++ src/DXF2GPT.py
@@ -44,6 +44,13 @@ class DXF2GPT:
         Units are in mm. Default deposition size is 1.75mm and build volume is 546.1mm x 546.1mm x 508mm.
         '''
         
+        # === Helper Functions ===
+        # https://stackoverflow.com/questions/25585401/travelling-salesman-in-scipy
+        # Calculate the euclidian distance in n-space of the route r traversing cities c, ending at the path start.
+        self.path_distance = lambda r,c: np.sum([np.linalg.norm(c[r[p]]-c[r[p-1]]) for p in range(len(r))])
+        # Reverse the order of all elements from element i to element k in array r.
+        self.two_opt_swap = lambda r,i,k: np.concatenate((r[0:i],r[k:-len(r)+i-1:-1],r[k+1:len(r)]))
+        
         # Filament Extruder default size 1.75mm
         self.cell_size = cell_size
         # Build volume size 21.5 inch x 21.5 inch x 20 inch
@@ -82,10 +89,8 @@ class DXF2GPT:
             for j in range(len(self.grid[0])):
                 self.dict[count] = (j, i)
                 count += 1
-        
-        # print(self.dict)
 
-    def bresenham_line(self, x1, y1, x2, y2):
+    def bresenham_line(self, x1, y1, x2, y2)->list:
         points = []
         dx = abs(x2 - x1)
         dy = abs(y2 - y1)
@@ -140,7 +145,7 @@ class DXF2GPT:
                 row.append(0)
             self.grid.append(row)
     
-    def csv2gridkeys(self):
+    def csv2gridkeys(self)->list:
         # Read the CSV file
         data = pd.read_csv(self.dir_path + "/csv_files/coordinates.csv")
         
@@ -230,17 +235,26 @@ class DXF2GPT:
         else:
             pass
 
-    def two_opt(self, dist_matrix, tour):
-        n = len(tour)
-        for i in range(n - 1):
-            for j in range(i + 2, n + (i == 0)):
-                if j >= n: # We have to mod j with n because of the edge case where i == 0
-                    j -= n
-                if dist_matrix[tour[i]][tour[i - 1]] + dist_matrix[tour[j]][tour[(j + 1) % n]] > dist_matrix[tour[i]][tour[j]] + dist_matrix[tour[i - 1]][tour[(j + 1) % n]]:
-                    tour[i + 1:j + 1] = reversed(tour[i + 1:j + 1])
-        return tour
+    def two_opt(self, cities, improvement_threshold): # 2-opt Algorithm adapted from https://en.wikipedia.org/wiki/2-opt
+        route = np.arange(cities.shape[0]) # Make an array of row numbers corresponding to cities.
+        improvement_factor = 1 # Initialize the improvement factor.
+        best_distance = self.path_distance(route,cities) # Calculate the distance of the initial path.
+        while improvement_factor > improvement_threshold: # If the route is still improving, keep going!
+            distance_to_beat = best_distance # Record the distance at the beginning of the loop.
+            for swap_first in range(1,len(route)-2): # From each city except the first and last,
+                # print('Swap First:', swap_first)
+                for swap_last in range(swap_first+1,len(route)): # to each of the cities following,
+                    # print('Swap Last:', swap_last)
+                    new_route = self.two_opt_swap(route,swap_first,swap_last) # try reversing the order of these cities
+                    new_distance = self.path_distance(new_route,cities) # and check the total distance with this modification.
+                    if new_distance < best_distance: # If the path distance is an improvement,
+                        route = new_route # make this the accepted best route
+                        best_distance = new_distance # and update the distance corresponding to this route.
+            improvement_factor = 1 - best_distance/distance_to_beat # Calculate how much the route has improved.
+            print('Improvement factor:', improvement_factor) # Print out the improvement factor.
+        return route # When the route is no longer improving substantially, stop searching and return the route.
 
-    def decode_grid(self, keys):
+    def decode_grid(self, keys, improvement_threshold=0.01):
         # decoding from grid (keys build the grid)
         # Use the keys to update the grid
         for i in keys:
@@ -249,40 +263,29 @@ class DXF2GPT:
         
         x_vals = []
         y_vals = []
-        
-        # use nearest neighbor to connect the points
-        
         # get the x and y coordinates of the grid
         points = [self.dict[key] for key in keys]
         # scale the points to the build volume
         points = [(point[0] * self.cell_size, point[1] * self.cell_size) for point in points]
-
-        # calculate the distance matrix
-        dist_matrix = distance.cdist(points, points, 'euclidean')
-
-        # start from the first point
-        current_point = 0
-        path = [current_point]
-
-        # set of unvisited points
-        unvisited = set(range(1, len(points)))
-
-        while unvisited:
-            # find the nearest unvisited point
-            nearest_point = min(unvisited, key=lambda point: dist_matrix[current_point][point])
-            unvisited.remove(nearest_point)
-            path.append(nearest_point)
-            current_point = nearest_point
-            
-        # path = self.two_opt(dist_matrix, path)
-        # print(path)
         
-        # close the path at the starting point
-        path.append(path[0])
+        # === Traveling Salesman Problem ===
+        # https://stackoverflow.com/questions/25585401/travelling-salesman-in-scipy
         
-        # get the x and y values of the points in the path
-        x_vals = [points[i][0] for i in path]
-        y_vals = [points[i][1] for i in path]
+        # Convert points to a numpy array
+        cities = np.array(points)
+        
+        print('Calculating the best route... This may take some time...') # future work: look into parallelizing this? Other optimization algorithms?
+        # Use the two_opt function to find a better route
+        route_indices = self.two_opt(cities, improvement_threshold)
+        
+        # Get the points in the order given by the route
+        route = cities[route_indices]
+        
+        # put first index of route at the end to close the loop
+        route = np.append(route, [route[0]], axis=0)
+        
+        x_vals = [point[0] for point in route]
+        y_vals = [point[1] for point in route]
 
         return x_vals, y_vals
               
@@ -309,7 +312,7 @@ class DXF2GPT:
 if __name__ == '__main__':
     # initialize the class object
     d2g = DXF2GPT()
-    d2g.cleanJSONL() # uncomment me if you want to delete the jsonl file before running the script (will generate new data)
+    d2g.cleanJSONL() # uncomment me if you want to delete the jsonl file before running the script (will generate new data each time)
     
     # ======================== Get user Input ========================
     
@@ -385,8 +388,11 @@ if __name__ == '__main__':
             print('Processing:', dxf_file)
             d2g = DXF2GPT()
             d2g.read_dxf2csv(file_path=dxf_file)
+            print('CSV file generated.')
             keys = d2g.csv2gridkeys()
+            print('Keys generated.')
             x, y = d2g.decode_grid(keys)
+            print('Grid decoded.')
             
             if debug == True:
                 print('Max X:', max(x))
@@ -398,7 +404,7 @@ if __name__ == '__main__':
             d2g.write_JSONL(keys, dxf_name=dxf_file.split('/')[-1].split('.')[0])
             
     elif batch == False:
-        dxf_file = '/Triangle_small.dxf' # Edit me to change the dxf file you want to process
+        dxf_file = '/Complex_II.dxf' # Edit me to change the dxf file you want to process
         d2g.read_dxf2csv(file_path=dxf_file)
         keys = d2g.csv2gridkeys()
         x, y = d2g.decode_grid(keys)
